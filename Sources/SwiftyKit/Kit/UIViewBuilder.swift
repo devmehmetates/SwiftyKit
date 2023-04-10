0299d8547370fadce19fe28c8e018851417efa33
Mehmet Ateş  <Mehmet1095@icloud.com>
2023-04-10 22:53:55 +0300

    UIView builder created

---
+++ Sources/SwiftyKit/Kit/UIViewBuilder.swift
@@ -0,0 +1,12 @@
+//
+//  UIViewBuilder.swift
+//  
+//
+//  Created by Mehmet Ateş on 10.04.2023.
+//
+
+import UIKit
+
+public final class UIViewBuilder: UIView, ViewBuilderProtocol {
+    public var content: UIView { self }
+}
