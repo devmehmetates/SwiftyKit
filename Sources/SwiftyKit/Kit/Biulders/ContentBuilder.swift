a08a65b784d1fb8ac1f5edbc1cfb5f4fc3c677c6
Mehmet Ateş  <Mehmet1095@icloud.com>
2023-04-14 22:45:19 +0300

    content builder updated

--- Sources/SwiftyKit/Kit/Biulders/ContentBuilder.swift
+++ Sources/SwiftyKit/Kit/Biulders/ContentBuilder.swift
@@ -9,18 +9,30 @@ import UIKit
 
 @resultBuilder
 final class ContentBuilder {
-    static func buildBlock(_ components: UIView...) -> [UIView] {
-        components
-    }
-    
-    static func buildEither(first component: UIView) -> [UIView] {
-        [component]
+    static func buildBlock(_ components: [UIView]...) -> [UIView] {
+        components.flatMap { $0 }
     }
 
-    static func buildEither(second component: UIView) -> [UIView] {
-        [component]
+    static func buildExpression(_ expression: UIView) -> [UIView] {
+        [expression]
     }
-    
+
+    static func buildExpression(_ expression: [UIView]) -> [UIView] {
+        expression
+    }
+
+    static func buildOptional(_ components: [UIView]?) -> [UIView] {
+        components ?? []
+    }
+
+    static func buildEither(first components: [UIView]) -> [UIView] {
+        components
+    }
+
+    static func buildEither(second components: [UIView]) -> [UIView] {
+        components
+    }
+
     static func buildArray(_ components: [[UIView]]) -> [UIView] {
         components.flatMap { $0 }
     }
