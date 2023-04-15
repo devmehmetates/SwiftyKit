a6e42dca1c574a39ba87b77a088dfe76bc2b960b
Mehmet Ateş  <Mehmet1095@icloud.com>
2023-04-16 00:59:05 +0300

    Vertical Stack documentation added

--- Sources/SwiftyKit/Kit/Bases/VerticalStack.swift
+++ Sources/SwiftyKit/Kit/Bases/VerticalStack.swift
@@ -7,7 +7,21 @@
 
 import UIKit
 
+/// A custom UIStackView subclass that stacks its arranged subviews vertically.
+///
+/// Example usage:
+/// ```swift
+/// VerticalStack {
+///     UILabel("First Label")
+///     UILabel("Second Label")
+///     UILabel("Third Label")
+/// }
+/// ```
 public final class VerticalStack: UIStackView {
+    
+    /// Initializes a new instance of `VerticalStack` with the given subviews.
+    ///
+    /// - Parameter content: A closure that returns an array of `UIView` objects to be arranged vertically.
     public convenience init(@ContentBuilder _ content: () -> [UIView]) {
         self.init(frame: .zero)
         self.axis(.vertical)
