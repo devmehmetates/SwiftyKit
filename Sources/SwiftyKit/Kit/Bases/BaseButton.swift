2ec731ad1cd1a1199aac485472dbced1a51ef3bc
Mehmet Ateş  <Mehmet1095@icloud.com>
2023-04-16 00:58:07 +0300

    base button documentation added

--- Sources/SwiftyKit/Kit/Bases/BaseButton.swift
+++ Sources/SwiftyKit/Kit/Bases/BaseButton.swift
@@ -7,9 +7,27 @@
 
 import UIKit
 
+/// A custom `UIButton` subclass with the ability to set a closure that will be called when the button is clicked.
+///
+/// Example usage:
+///
+/// ```swift
+/// BaseButton(type: .system)
+///     .didClick {
+///         print("Button clicked!")
+///     }
+///     .setTitle("Button Title")
+///     .padding(.horizontal)
+/// ```
 public final class BaseButton: UIButton {
+    
+    /// The closure that will be called when the button is clicked.
     private var didClick: (() -> Void)?
     
+    /// Sets the closure to be called when the button is clicked and returns itself, allowing for chaining.
+    ///
+    /// - Parameter completion: The closure to be called when the button is clicked.
+    /// - Returns: The `BaseButton` instance itself.
     @discardableResult
     public func didClick(_ completion: @escaping () -> Void) -> Self {
         self.didClick = completion
@@ -17,8 +35,8 @@ public final class BaseButton: UIButton {
         return self
     }
     
-    @objc
-    private func onTap() {
+    /// The function called when the button is clicked.
+    @objc private func onTap() {
         didClick?()
     }
 }
