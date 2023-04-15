297ac979a1eb9f1e48163b8e5d617b331dcab29b
Mehmet Ateş  <Mehmet1095@icloud.com>
2023-04-16 00:58:47 +0300

    Horizontal stack documentation added

--- Sources/SwiftyKit/Kit/Bases/HorizontalStack.swift
+++ Sources/SwiftyKit/Kit/Bases/HorizontalStack.swift
@@ -7,7 +7,20 @@
 
 import UIKit
 
+/// A custom UIStackView subclass that stacks its arranged subviews horizontally.
+/// Example usage:
+/// ```swift
+/// HorizontalStack {
+///     UILabel("First Label")
+///     UILabel("Second Label")
+///     UILabel("Third Label")
+/// }
+/// ```
 public final class HorizontalStack: UIStackView {
+    
+    /// Initializes a new instance of `HorizontalStack` with the given subviews.
+    ///
+    /// - Parameter content: A closure that returns an array of `UIView` objects to be arranged horizontally.
     public convenience init(@ContentBuilder _ content: () -> [UIView]) {
         self.init(frame: .zero)
         self.axis(.horizontal)
