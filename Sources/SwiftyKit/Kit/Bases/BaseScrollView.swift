b732c7e7e65e928b581053543d0290b2c07fa9f5
Mehmet Ateş  <Mehmet1095@icloud.com>
2023-04-16 00:58:24 +0300

    base scrollview documentation added

--- Sources/SwiftyKit/Kit/Bases/BaseScrollView.swift
+++ Sources/SwiftyKit/Kit/Bases/BaseScrollView.swift
@@ -6,10 +6,41 @@
 //
 
 import UIKit
-
+/// A custom `UIScrollView` subclass that simplifies the process of adding a stack of views as subviews.
+/// 
+/// Example usage:
+///
+/// ```swift
+/// BaseScrollView {
+///     UILabel("First Label")
+///     UILabel("Second Label")
+///     UILabel("Third Label")
+/// }
+/// ```
+///
+/// Recommended  usage:
+///
+/// ```swift
+/// BaseScrollView {
+///     VerticalStack {
+///         UILabel("First Label")
+///         UILabel("Second Label")
+///         UILabel("Third Label")
+///     }
+/// }
+/// ```
 public final class BaseScrollView: UIScrollView {
+    
+    /// The stack view that will hold the content.
     private let contentView = VerticalStack()
     
+    /// Initializes the scroll view with a vertical stack of views as subviews.
+    ///
+    /// - Parameters:
+    ///   - insets: The edge insets of the content view from the scroll view's bounds.
+    ///   - insetPadding: The padding between the content view and the scroll view's bounds.
+    ///   - content: A closure that returns an array of views to be added as subviews of the scroll view.
+    ///
     public convenience init(_ insets: Edges.Set = .all, _ insetPadding: CGFloat = 0, @ContentBuilder _ content: () -> [UIView]) {
         self.init(frame: .zero)
         content().forEach { contentView.addArrangedView($0) }
