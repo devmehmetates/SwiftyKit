8aa0a651c5e9197bf30046b9df41fca225ae1aef
Mehmet Ateş  <Mehmet1095@icloud.com>
2023-04-16 00:59:22 +0300

    Edges Set documentation added

--- Sources/SwiftyKit/Kit/Enums/Edges.swift
+++ Sources/SwiftyKit/Kit/Enums/Edges.swift
@@ -8,22 +8,31 @@
 import UIKit
 import SwiftUI
 
+/// An enum representing the edges of a rectangle or a view.
 public enum Edges: Int8, CaseIterable {
     case top
     case leading
     case bottom
     case trailing
     
+    /// An option set representing a set of edges.
     public struct Set: OptionSet {
         public init(rawValue: Int8) { self.rawValue = rawValue }
         public let rawValue: Int8
 
-        public static let top             = Set(rawValue: 1 << 0)
-        public static let leading         = Set(rawValue: 1 << 1)
-        public static let bottom          = Set(rawValue: 1 << 2)
-        public static let trailing        = Set(rawValue: 1 << 3)
+        /// The top edge.
+        public static let top = Set(rawValue: 1 << 0)
+        /// The leading edge.
+        public static let leading = Set(rawValue: 1 << 1)
+        /// The bottom edge.
+        public static let bottom = Set(rawValue: 1 << 2)
+        /// The trailing edge.
+        public static let trailing = Set(rawValue: 1 << 3)
+        /// The horizontal edges.
         public static let horizontal: Set = [.leading, .trailing]
-        public static let vertical: Set   = [.top, .bottom]
-        public static let all: Set        = [.leading, .trailing, .top, .bottom]
+        /// The vertical edges.
+        public static let vertical: Set = [.top, .bottom]
+        /// All edges.
+        public static let all: Set = [.leading, .trailing, .top, .bottom]
     }
 }
