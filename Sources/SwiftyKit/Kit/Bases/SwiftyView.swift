259629a6871a860305dbb4ede772e8ede1a4a4a3
Mehmet Ateş  <Mehmet1095@icloud.com>
2023-12-10 16:45:21 +0300

    UIView usage changed to SwiftyView

---
+++ Sources/SwiftyKit/Kit/Bases/SwiftyView.swift
@@ -0,0 +1,39 @@
+//
+//  SwiftyView.swift
+//  
+//
+//  Created by Mehmet Ateş on 10.12.2023.
+//
+
+import UIKit
+
+public extension SwiftyViewInferface where Self: UIView {
+    func reDraw() {
+        cleanDrawed()
+        draw()
+    }
+    
+    func cleanDrawed() {
+        content.removeFromSuperview()
+        self.subviews.forEach { $0.removeFromSuperview() }
+    }
+
+    func draw() {
+        content
+            .embedTo(self)
+            .fillToSuperView()
+    }
+}
+
+open class SwiftyView: UIView, SwiftyViewInferface {
+    public var content: UIView { self }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        draw()
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
